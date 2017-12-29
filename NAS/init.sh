754158b541bab0d3ff52565b56316ce789e43b9d
zby <497256303zby@gmail.com>
2017-12-30 01:51:47 +0800

    Add init.sh

--- NAS/init.sh
+++ NAS/init.sh
@@ -1,12 +1,35 @@
 #!/bin/bash
-# filename: test.sh
-
+#$1:NASµÄipµØÖ·
 
 mkdir /log
 chmod 777 /log
 
 rm -rf /etc/samba/smb.conf
 touch /etc/samba/smb.conf
+
+sed -i "s/localhost/${1}/" ./vfm-admin/config.php
+
+rm -rf /etc/httpd/conf/httpd.conf
+cp ./httpd.conf /etc/httpd/conf/httpd.conf
+
+rm -rf /etc/sysconfig/network-scripts/ifcfg-ens32
+cp ./ifcfg-ens32 /etc/sysconfig/network-scripts/ifcfg-ens32
+
+ip=`sed "/IPADDR/"q /etc/sysconfig/network-scripts/ifcfg-ens32|sed -n '$='`
+gateway=`sed "/GATEWAY/"q /etc/sysconfig/network-scripts/ifcfg-ens32|sed -n '$='`
+dns=`sed "/DNS/"q /etc/sysconfig/network-scripts/ifcfg-ens32|sed -n '$='`
+sed -i -e '${ip}c/IPADDR="$1"' /etc/sysconfig/network-scripts/ifcfg-ens32
+sed -i -e '${gateway}c/GATEWAY="${1%.*}.1"' /etc/sysconfig/network-scripts/ifcfg-ens32
+sed -i -e '${dns}c/DNS1="${1%.*}.1"' /etc/sysconfig/network-scripts/ifcfg-ens32
+service network restart
+
+cp vfm-admin /home/vfm-admin
+cp uploads /home/uploads
+cp index.php /home/index.php
+cp vfm-thumb.php /home/vfm-thumb.php
+
+chmod -R 777 /home/vfm-admin /home/uploads /home/index.php /home/vfm-thumb.php
+
 
 echo "# See smb.conf.example for a more detailed config file or
 # read the smb.conf manpage.
@@ -56,7 +79,7 @@ echo "# See smb.conf.example for a more detailed config file or
 ">>/etc/samba/smb.conf	
 
 touch /etc/samba/smbpasswd
-(echo "password";usleep 1000;echo "password")|--stdin smbpasswd -a root 
+(echo "password";usleep 1000;echo "password")|smbpasswd -a root
 
 systemctl disable firewalld
 systemctl stop firewalld
