c4503bc112056f01bbf245330c96d04ce5955c29
Funk <wy98926@gmail.com>
2017-12-30 17:42:19 +0800

    12-30 17:42 Add Forwarding shell scripts

--- ForwardingCenter/dtc.sh
+++ ForwardingCenter/dtc.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-echo G1551265|mysql -u G1551265 -p
-
-create database G1551265
-
 echo G1551265|mysql -u G1551265 -p G1551265 < /home/G1551265/nas_user.sql
 echo G1551265|mysql -u G1551265 -p G1551265 < /home/G1551265/user_auth.sql
 echo G1551265|mysql -u G1551265 -p G1551265 < /home/G1551265/user_info.sql
